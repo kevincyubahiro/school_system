99df7fa3907f0d22a7a8f83f80d446b23a0d2c8c
kevin cyubahiro <162144436+kevincyubahiro@users.noreply.github.com>
2024-10-08 18:07:01 +0300

    Add files via upload

---
+++ Gikonko_technical_secondary_school_system/insert.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   
+    
+    <link rel="stylesheet" href="style.css">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+    <div class="form">
+    <h2>Login form</h2>
+   
+    <form action="" method="post" autocomplete="off">
+
+    <input type="text"  name="username" placeholder="Enter your Username" required><br><br>
+    <input type="password" name="password" placeholder="Enter your Password"required><br><br>
+    <button name="submit">login</button><br>
+    <a href="login.php">Create new account</a>
+    
+    </form>
+    </div>
+</body>
+</html>
+<?php
+include("conn.php");
+session_start();
+if(isset($_POST['submit'])){
+    $username=$_POST['username'];
+    $password=$_POST['password'];
+    $login=mysqli_query($conn,"SELECT * FROM trainers WHERE username='$username' AND `password`='$password'");
+    $count=mysqli_num_rows($login);
+    if($count){
+        $_SESSION['username']=$username;
+        $_SESSION['password']=$password;
+}
+else{
+    echo "please try to create  new account";
+}
+
+}
+?>
